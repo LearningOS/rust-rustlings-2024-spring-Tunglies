e6ee469ef3261c06559a2d3d8e9230daeeb9c1e6
Tunglies <tunglies@outlook.com>
2024-04-08 18:22:10 +0800

    46

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,13 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> String {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+            }
         }
         output
     }
@@ -45,7 +47,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    // use crate::my_module::transformer;
+    use my_module::transformer;
     use super::Command;
 
     #[test]

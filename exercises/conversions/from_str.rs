5bb9a9d1cd714e20e5d2230f3803781672799e18
Tunglies <tunglies@outlook.com>
2024-04-09 14:44:36 +0800

    92

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,29 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        };
+
+        let mut words = s.split(",");
+        if words.clone().count() > 2 {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        let name = words.next().ok_or(ParsePersonError::NoName)?;
+        let age = words.next().ok_or(ParsePersonError::BadLen)?;
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let _age = age.parse::<usize>().map_err(ParsePersonError::ParseInt)?;
+        
+
+        return Ok(Person {
+            name: name.into(),
+            age: _age,
+        });
     }
 }
 
